730659ce83c560917ea34085d51afef0335632d8
nobiot <noboru.ota@gmail.com>
2019-09-23 22:40:58 +0200

    google tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-109049728-1`,
       },
     },
     `gatsby-plugin-feed`,
