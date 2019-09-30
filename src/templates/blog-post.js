0b0a448f4fb2a1f69147851d2dd6534d0f5a5466
nobiot <noboru.ota@gmail.com>
2019-09-30 16:29:18 +0200

    blog-post.js now function not class

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,73 +1,64 @@
-import React from "react"
-import { graphql } from "gatsby"
-import "./footnote.css"
+import React from 'react'
+import { graphql } from 'gatsby'
+import './footnote.css'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm, scale } from "../utils/typography"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { rhythm, scale } from '../utils/typography'
 
-import Img from "gatsby-image"
-const Md = require("markdown-it")().use(require("markdown-it-footnote"))
+import Img from 'gatsby-image'
+const Md = require('markdown-it')().use(require('markdown-it-footnote'))
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    let featuredImgFluid = post.frontmatter.featured_image.childImageSharp.fluid
+export default ({ location, data }) => {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata.title
+  const featuredImgFluid = post.frontmatter.featured_image.childImageSharp.fluid
+  const ingredients = post.frontmatter.recipe.ingredients
+  const instructions = post.frontmatter.recipe.instructions
+  const html = instructions
+    ? `<h2>Steps</h2>` + Md.render(instructions) + post.html
+    : post.html
 
-    let ingredients = post.frontmatter.recipe.ingredients
-    let instructions = post.frontmatter.recipe.instructions
-    let html
-
-    if (instructions) {
-      html = `<h2>Steps</h2>` + Md.render(instructions) + post.html
-    } else {
-      html = post.html
-    }
-  
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <article>
-          <Img fluid={featuredImgFluid} />
-          <header>
-            <h1
-              style={{
-                marginTop: rhythm(1),
-                marginBottom: 0,
-              }}
-            >
-              {post.frontmatter.title}
-            </h1>
-            <p
-              style={{
-                ...scale(-1 / 5),
-                display: `block`,
-                marginBottom: rhythm(1),
-              }}
-            >
-              {post.frontmatter.date}
-            </p>
-          </header>
-          <h2>Ingredients</h2>
-          <section dangerouslySetInnerHTML={{ __html: Md.render(ingredients) }} />
-          <section dangerouslySetInnerHTML={{ __html: html }} />
-          <hr
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <article>
+        <Img fluid={featuredImgFluid} />
+        <header>
+          <h1
             style={{
-              marginBottom: rhythm(1),
+              marginTop: rhythm(1),
+              marginBottom: 0
             }}
-          />
-        </article>
+          >
+            {post.frontmatter.title}
+          </h1>
+          <p
+            style={{
+              ...scale(-1 / 5),
+              display: `block`,
+              marginBottom: rhythm(1)
+            }}
+          >
+            {post.frontmatter.date}
+          </p>
+        </header>
+        <h2>Ingredients</h2>
+        <section dangerouslySetInnerHTML={{ __html: Md.render(ingredients) }} />
+        <section dangerouslySetInnerHTML={{ __html: html }} />
+        <hr
+          style={{
+            marginBottom: rhythm(1)
+          }}
+        />
+      </article>
 
-      </Layout>
-    )
-  }
+    </Layout>
+  )
 }
-
-export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
